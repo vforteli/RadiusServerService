65da19045919a9b5ad27e4f5c951afef2f5b1d24
vforteli <verner@fortelius.net>
2016-08-12 19:53:07 +0300

    stuff

--- RadiusServerService.cs
+++ RadiusServerService.cs
@@ -18,7 +18,8 @@ namespace Flexinets.Radius
     public partial class RadiusServerService : ServiceBase
     {
         private FlexinetsEntitiesFactory _contextFactory;
-        private RadiusServer _rs;
+        private RadiusServer _rsauth;
+        private RadiusServer _rsacct;
         private readonly ILog _log = LogManager.GetLogger(typeof(RadiusServerService));
 
 
@@ -32,17 +33,38 @@ namespace Flexinets.Radius
             log4net.Config.XmlConfigurator.Configure();
             try
             {
+                _log.Info("Reading configuration");
                 _contextFactory = new FlexinetsEntitiesFactory(CloudConfigurationManager.GetSetting("SQLConnectionString"));
-
                 var path = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + "\\dictionary";  // todo hurgh
                 var dictionary = new RadiusDictionary(path);
-
                 var port = Convert.ToInt32(CloudConfigurationManager.GetSetting("Port"));
-                var secret = CloudConfigurationManager.GetSetting("secret");
+                var ipassSecret = CloudConfigurationManager.GetSetting("ipasssecret");
+                var mbbSecret = CloudConfigurationManager.GetSetting("mbbsecret");
+                var disconnectCheckerPath = CloudConfigurationManager.GetSetting("DisconnectCheckerPath");
+                var welcomeSenderPath = CloudConfigurationManager.GetSetting("WelcomeSenderPath");
+                var apiUrl = CloudConfigurationManager.GetSetting("ApiUrl");
                 _log.Info("Configuration read");
-                _rs = new RadiusServer(new IPEndPoint(IPAddress.Any, port), dictionary);
-                _rs.AddPacketHandler(IPAddress.Parse("127.0.0.1"), secret, new iPassPacketHandler(_contextFactory));
-                _rs.Start();
+
+
+                _rsauth = new RadiusServer(new IPEndPoint(IPAddress.Any, port), dictionary);
+                _rsacct = new RadiusServer(new IPEndPoint(IPAddress.Any, port + 1), dictionary);    // todo, good grief...
+
+                var ipassPacketHandler = new iPassPacketHandler(_contextFactory);
+                _rsauth.AddPacketHandler(IPAddress.Parse("127.0.0.1"), ipassSecret, ipassPacketHandler);
+                _rsacct.AddPacketHandler(IPAddress.Parse("127.0.0.1"), ipassSecret, ipassPacketHandler);
+
+
+                var networkIdProvider = new NetworkIdProvider(_contextFactory, apiUrl);
+                var mbbPacketHandler = new MobileDataPacketHandler(_contextFactory, networkIdProvider, disconnectCheckerPath, welcomeSenderPath);
+
+                _rsauth.AddPacketHandler(IPAddress.Parse("10.50.0.253"), mbbSecret, mbbPacketHandler);
+                _rsauth.AddPacketHandler(IPAddress.Parse("10.50.0.254"), mbbSecret, mbbPacketHandler);
+
+                _rsacct.AddPacketHandler(IPAddress.Parse("10.50.0.253"), mbbSecret, mbbPacketHandler);
+                _rsacct.AddPacketHandler(IPAddress.Parse("10.50.0.254"), mbbSecret, mbbPacketHandler);
+
+                _rsauth.Start();
+                _rsacct.Start();
             }
             catch (Exception ex)
             {
@@ -53,8 +75,10 @@ namespace Flexinets.Radius
 
         protected override void OnStop()
         {
-            _rs.Stop();
-            _rs.Dispose();
+            _rsauth.Stop();
+            _rsauth.Dispose();
+            _rsacct.Stop();
+            _rsacct.Dispose();
         }
     }
 }
